19555b32dabd3202d66daae60fdf8c04ba283bd3
Xavier Brin <x.brin@hotmail.fr>
2017-11-04 11:27:41 +0000

    Updated code to ES6

--- index.js
+++ index.js
@@ -1,72 +1,68 @@
-var http = require("http");
-var https = require("https");
+const http = require("http")
+const https = require("https")
 
-function queryCryptocompareApi(date) {
-  return new Promise(function(resolve, reject) {
-    var options = {
-      host: 'min-api.cryptocompare.com',
-      port: 443,
-      path: '/data/pricehistorical?fsym=ETH&tsyms=USD&ts=' + date,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-    var port = options.port == 443 ? https : http;
-    var req = port.request(options, function(res) {
-      var output = '';
-      res.setEncoding('utf8');
-
-      res.on('data', function (chunk) {
-        output += chunk;
-      });
-
-      res.on('end', function() {
-        var data = JSON.parse(output);
-        resolve({
-          statusCode: res.statusCode,
-          data: data
-        });
-      });
-    });
-
-    req.on('error', function(err) {
-      reject(err);
-    });
-
-    req.end();
-  });
-};
-
-function roi(buyDate, sellDate, amount) {
-  try {
-    buyDate = parseInt(buyDate); 
-		sellDate = parseInt(sellDate); 
-		amount = parseInt(amount); 
-		if(sellDate > buyDate) {
-      return queryCryptocompareApi(buyDate).then(function(buyDateResult) {
-        if(buyDateResult.statusCode == 200) {
-          console.log(buyDateResult);
-          return queryCryptocompareApi(sellDate).then(function(sellDateResult) {
-            console.log(sellDateResult);
-            var gain = sellDateResult.data.ETH.USD * amount;
-            var cost = buyDateResult.data.ETH.USD * amount;
-            var roi = ( gain - cost ) / cost;
-            console.log('\nReturn on investment : ', roi);
-            return roi;
-          }).catch(function(exception) { console.error(exception); });
-        } else {
-          throw new Error('cannot connect to min-api.cryptocompare.com');
-        }
-      }).catch(function(exception) { console.error(exception); });
-		} else {
-			throw new Error('sellDate must be greater than buyDate');
+const queryCryptocompareApi = (date) => {
+	return new Promise((resolve, reject) => {
+		const options = {
+			host: 'min-api.cryptocompare.com',
+			port: 443,
+			path: '/data/pricehistorical?fsym=ETH&tsyms=USD&ts=' + date,
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
 		}
-	} catch(Exception) {
-		throw new Error('invalid arguments');
+		const port = options.port == 443 ? https : http
+		const req = port.request(options, (res) => {
+			let output = ''
+			res.setEncoding('utf8')
+			res.on('data', (chunk) => {
+				output += chunk
+			})
+			res.on('end', function() {
+				var data = JSON.parse(output)
+				resolve({
+					statusCode: res.statusCode,
+					data: data
+				})
+			})
+		})
+		req.on('error', (err) => {
+			reject(err)
+		})
+		req.end()
+	})
+}
+
+const roi = (buyDate, sellDate, amount) => {
+	try {
+		buyDate = parseInt(buyDate)
+		sellDate = parseInt(sellDate)
+		amount = parseInt(amount)
+		if (sellDate > buyDate) {
+			return queryCryptocompareApi(buyDate).then(buyDateResult => {
+				if(buyDateResult.statusCode == 200) {
+					console.log('Buy date values : ', buyDateResult)
+					return queryCryptocompareApi(sellDate).then(sellDateResult => {
+						console.log('Sell date values : ', sellDateResult)
+						let gain = sellDateResult.data.ETH.USD * amount
+						let cost = buyDateResult.data.ETH.USD * amount
+						let roi = ( gain - cost ) / cost
+						console.log('\nReturn on investment : ', roi)
+						return roi
+					}).catch(exception => { console.error(exception) })
+        			} else {
+          				throw new Error('cannot connect to min-api.cryptocompare.com')
+        			}
+			}).catch(exception => { console.error(exception) })
+		} else {
+			throw new Error('sellDate must be greater than buyDate')
+		}
+	} catch(exception) {
+		console.error(exception)
 	}
 }
 
-roi(process.argv[2], process.argv[3], process.argv[4]);
+roi(process.argv[2], process.argv[3], process.argv[4])
 
-module.exports = roi;
+module.exports = roi
